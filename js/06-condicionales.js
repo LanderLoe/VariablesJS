dc5f64690cc060dba80eb7cb276318466793f207
Leandro Lobos <leonelleandro13@gmail.com>
2024-07-03 03:19:09 -0300

    creo el programa 6 con condicionales

--- js/06-condicionales.js
+++ js/06-condicionales.js
@@ -4,3 +4,12 @@ Ejemplo:
 input: 15 , 3
 Output: El 15 es el número más grande
 */
+
+const numero1 = parseInt(prompt('Escribe un número'))
+const numero2 = parseInt(prompt('Escribe otro número'))
+
+if(numero1 >= numero2){
+    document.write(`<h2> El número mayor es: ${numero1}</h2>`)
+}else if(numero1 <= numero2){
+    document.write(`<h2> El número mayor es: ${numero2}</h2>`)
+}
