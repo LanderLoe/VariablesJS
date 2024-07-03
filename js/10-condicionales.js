5494f4e576ab93dd04e4bf12665360185f08776c
Leandro Lobos <leonelleandro13@gmail.com>
2024-07-03 05:13:53 -0300

    creo el programa 10 con condicionales

--- js/10-condicionales.js
+++ js/10-condicionales.js
@@ -4,3 +4,17 @@ Ejemplo:
 input: 20
 Output: El 20 es divisible por 2.
 */
+
+const numero = parseInt(prompt('Ingresa un número'))
+
+if(numero%2 == 0){
+    alert(`El número: ${numero} es divisible por 2`)
+}else if(numero%3 == 0){
+    alert(`El número: ${numero} es divisible por 3`)
+}else if(numero%5 == 0){
+    alert(`El número: ${numero} es divisible por 5`)
+}else if(numero%7 == 0){
+    alert(`El número: ${numero} es divisible por 7`)
+}else{
+    alert(`El número: ${numero} no es divisible por 2, 3, 5 o 7`)
+}
