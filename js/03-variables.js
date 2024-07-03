3658e497038a17d07348076b8a23763d9df51545
Leandro Lobos <leonelleandro13@gmail.com>
2024-07-03 02:28:39 -0300

    creo el programa 3 con variables

--- js/03-variables.js
+++ js/03-variables.js
@@ -1,1 +1,6 @@
 // Escribe un programa de una sola línea que escriba en la pantalla el resultado de sumar 3 + 5.
+const numero1 = 3
+const resultado = (numero1 + 5)
+
+document.write(`<h2> ${numero1} + 5`)
+document.write (`<h2>Resultado: ${resultado}<h2>`)
