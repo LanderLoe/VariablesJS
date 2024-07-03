41d5f0461c7193b7e8202ea9f97ef7eda22d9a45
Leandro Lobos <leonelleandro13@gmail.com>
2024-07-03 02:55:53 -0300

    creo el programa 5 con variables

--- js/05-variables.js
+++ js/05-variables.js
@@ -3,3 +3,9 @@
 Nota: Tener en cuenta la siguiente función: parseInt
 https://developer.mozilla.org/es/docs/Web/JavaScript/Referencia/Objetos_globales/parseInt
  */
+
+const numero1 = parseInt(prompt('Escribe un número'))
+const numero2 = parseInt(prompt('Escribe otro número'))
+const suma = (numero1 + numero2)
+
+document.write(`<h2> La suma de los números es: ${suma}</h2>`)
